5915b4afa25862cc52ad2bee94c43e1e64115094
TanishqJain2003 <tarunkumarjain7456999487@gmail.com>
2025-07-06 18:09:25 +0530

    Time: 4 ms (88.97%), Space: 58.6 MB (31.37%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,27 +1,47 @@
+// class Solution {
+//     public int findDuplicate(int[] arr) {
+//         int i = 0;
+//         while ( i < arr.length){
+//             if(arr[i]!= i+1){
+//                 int correct = arr[i]-1;
+//                 if(arr[i] != arr[correct]){
+//                     swap(arr, i , correct);
+//                 }
+//                 else{
+//                     return arr[i];
+//                 }
+//             }
+//                 else{
+//                     i++;
+//                 }
+//         }
+            
+//             return -1;
+//     }
+    
+//         void swap(int[] arr , int first , int second){
+//             int temp = arr[first];
+//             arr[first]= arr[second];
+//             arr[second] = temp;
+//         }
+// }
 class Solution {
     public int findDuplicate(int[] arr) {
-        int i = 0;
-        while ( i < arr.length){
-            if(arr[i]!= i+1){
-                int correct = arr[i]-1;
-                if(arr[i] != arr[correct]){
-                    swap(arr, i , correct);
-                }
-                else{
-                    return arr[i];
-                }
-            }
-                else{
-                    i++;
-                }
+        int slow = arr[0];
+        int fast = arr[0];
+        do{
+            slow = arr[slow];
+            fast = arr[arr[fast]];
         }
-            
-            return -1;
+        while(slow != fast);
+        fast = arr[0];
+         while(slow != fast){
+            slow = arr[slow];
+            fast = arr[fast];
+         }
+     return slow;
     }
-    
-        void swap(int[] arr , int first , int second){
-            int temp = arr[first];
-            arr[first]= arr[second];
-            arr[second] = temp;
-        }
+
 }
+
+        
