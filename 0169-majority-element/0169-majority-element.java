58a9edf7dcc09b5548eed11c97a9a6127dce50d3
TanishqJain2003 <tarunkumarjain7456999487@gmail.com>
2025-07-09 22:44:39 +0530

    Time: 1 ms (99.85%), Space: 53 MB (59.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,63 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+//     public int majorityElement(int[] nums) {
+//         // Arrays.sort(nums);
+//         // int n = nums.length;
+//         // return nums[n/2];
+//         int cnt = 0;
+//         int el = 0;
+//         int n = nums.length;
+//         for(int i = 0; i<n; i++){
+//         if(cnt == 0){
+//             cnt = 1;
+//             el = nums[i];
+//         }
+//         else if(el == nums[i]){
+//             cnt++;
+//         }
+//         else cnt--;
+//         }
+
+//         int cnt1 = 0;
+//         for(int i = 0; i < n; i++){
+//          if(el == nums[i]){
+//             cnt1++;
+//          }
+//          if(cnt1 > (n/2)){
+//             return el;
+//          }
+//          return -1;
+//         }
+//     }
+// }
+public int majorityElement(int[] nums) {
+    int cnt = 0;
+    int el = 0;
+    int n = nums.length;
+
+    // Phase 1: Find a candidate
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = nums[i];
+        } else if (el == nums[i]) {
+            cnt++;
+        } else {
+            cnt--;
+        }
     }
+
+    // Phase 2: Verify the candidate
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == el) {
+            cnt1++;
+        }
+    }
+
+    if (cnt1 > n / 2) {
+        return el;
+    }
+
+    return -1; // No majority element
+}
 }
